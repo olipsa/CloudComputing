48e933200352b373aae5fc8560be616929aa93da
Ondina <ondina_patricia@yahoo.com>
2022-03-15 16:45:54 +0200

    POST + PUT methods + Postman collection

--- Tema2/database.py
+++ Tema2/database.py
@@ -22,7 +22,6 @@ def select(table_name, **kwargs):
         sql += " WHERE "
         for col_name in kwargs:
             sql += f"{col_name}={kwargs[col_name]}"
-    print(sql)
     try:
         cursor.execute(sql)
         result = [dict(row) for row in cursor.fetchall()]
@@ -32,23 +31,84 @@ def select(table_name, **kwargs):
     return result
 
 
-def insert(table_name):
-    pass
+def get_next_id(table_name):
+    global con
+    cursor = con.cursor()
+    cursor.execute(f"SELECT * FROM {table_name} ORDER BY ID DESC LIMIT 1")
+    for row in cursor.fetchall():
+        return row['ID']+1
+
+
+def insert(table_name, data):
+    global con
+    cursor = con.cursor()
+    try:
+        cursor.execute(f"SELECT * FROM {table_name}")
+    except sqlite3.OperationalError:
+        # table not found
+        return 0
+    columns = ""
+    values = ""
+    for column in data:
+        columns += column+", "
+        try:
+            values += "\""+data[column]+"\", "
+        except TypeError:  # value is not a str
+            values += str(data[column]) + ", "
+    columns += "ID"
+    id = get_next_id(table_name)
+    values += str(id)
+    sql = f"INSERT INTO {table_name} ({columns}) VALUES({values})"
+    try:
+        cursor.execute(sql)
+        con.commit()
+        return id
+    except sqlite3.IntegrityError:  # item already exists
+        return -1
+    except sqlite3.OperationalError: # invalid query
+        return -2
+
+
+def update(table_name, id, data):
+    global con
+    cursor = con.cursor()
+    try:
+        cursor.execute(f"SELECT * FROM {table_name} WHERE ID={id}")
+    except sqlite3.OperationalError:
+        # table not found
+        return 0
+
+    sql = f"UPDATE {table_name} SET "
+    for column in data:
+        try:
+            sql += column + " = " + "\"" + data[column] + "\", "
+        except TypeError:  # value is not a str
+            sql += column + " = " + str(data[column]) + ", "
+    sql = sql[:-2]
+    sql += f" WHERE ID={id}"
+    print(sql)
+    try:
+        cursor.execute(sql)
+        con.commit()
+        return 1
+    except sqlite3.OperationalError:
+        return -1
+
 
 
 def delete(table_name, id):
     global con
     cursor = con.cursor()
-    cursor.execute(f"SELECT * FROM {table_name} WHERE ID={id}")
-    if not cursor.fetchall():
-        # item not found
-        return 0
     try:
-        sql = f"DELETE FROM {table_name} WHERE ID={id}"
-        cursor.execute(sql)
-        print(sql)
-        con.commit()
-        return 1
+        cursor.execute(f"SELECT * FROM {table_name} WHERE ID={id}")
+        if not cursor.fetchall():
+            # item not found
+            return 0
     except sqlite3.OperationalError:
         # table not found
         return 0
+
+    cursor.execute(f"DELETE FROM {table_name} WHERE ID={id}")
+    con.commit()
+    return 1
+
--- Tema2/methods.py
+++ Tema2/methods.py
@@ -1,5 +1,5 @@
 import json
-
+import cgi
 import database
 
 
@@ -16,10 +16,10 @@ def GET(server, path, parameters):
         collection = path_list[0]
         data = []
         if len(path_list) == 1:
-            # List all books available
+            # List all items available
             data = database.select(collection)
         elif len(path_list) == 2:
-            # List info about specific book -> with id = path_list[1]
+            # List info about specific item -> with id = path_list[1]
             data = database.select(collection, ID=path_list[1])
 
         if len(path_list) > 2 or len(data) == 0:  # if URL invalid or collection/item not found
@@ -35,11 +35,85 @@ def GET(server, path, parameters):
 
 
 def POST(server, path, parameters):
-    pass
+    type, pdict = cgi.parse_header(server.headers['content-type'])
+    body = server.rfile.read(int(server.headers['content-length']))
+    if type == 'application/json' and len(body) != 0:
+        data = json.loads(body.decode())
+        path_list = path.split('/')
+        path_list = list(filter(lambda a: a != '', path_list))
+        if len(path_list) == 0:  # homepage request
+            server.send_response(405)
+            server.send_header("Content-type", "text/html")
+            server.end_headers()
+        elif len(path_list) == 1:  # create new item on the provided collection
+            collection = path_list[0]
+            response = database.insert(collection, data)
+            if response == 0:  # collection not found
+                server.send_response(404)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+            elif response == -1:  # item already exists
+                server.send_response(409)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+            elif response == -2:  # invalid item
+                server.send_response(400)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+            else:
+                server.send_response(200)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+                server.wfile.write(bytes(server.path+"/"+str(response), 'utf-8'))
+        else:  # collection not found
+            server.send_response(404)
+            server.send_header("Content-type", "text/html")
+            server.end_headers()
+
+    elif type != 'application/json':  # type not implemented
+        server.send_response(501)
+        server.send_header("Content-type", "text/html")
+        server.end_headers()
+    else:  # no data to post
+        server.send_response(404)
+        server.send_header("Content-type", "text/html")
+        server.end_headers()
 
 
 def PUT(server, path, parameters):
-    pass
+    path_list = path.split('/')
+    path_list = list(filter(lambda a: a != '', path_list))
+    if len(path_list) == 0 or len(path_list) == 1:  # homepage or entire collection
+        server.send_response(405)
+        server.send_header("Content-type", "text/html")
+        server.end_headers()
+    else:
+        collection = path_list[0]
+        type, pdict = cgi.parse_header(server.headers['content-type'])
+        body = server.rfile.read(int(server.headers['content-length']))
+        data = json.loads(body.decode())
+        if type == 'application/json' and len(body) != 0:
+            response = database.update(collection, path_list[1], data)
+            if response == 0:  # collection not found
+                server.send_response(404)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+            elif response == -1:  # invalid item
+                server.send_response(400)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+            elif response == 1:
+                server.send_response(200)
+                server.send_header("Content-type", "text/html")
+                server.end_headers()
+        elif type != 'application/json':  # type not implemented
+            server.send_response(501)
+            server.send_header("Content-type", "text/html")
+            server.end_headers()
+        else:  # no data to post
+            server.send_response(404)
+            server.send_header("Content-type", "text/html")
+            server.end_headers()
 
 
 def DELETE(server, path, parameters):
